fd69c23b6ff1844a920958e190d3e17e29c45d87
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-07 04:23:13 +0000

    Create interfaces for passive and immediate interaction

---
+++ Model/Interfaces/IImmediateInteractble.cs
@@ -0,0 +1,7 @@
+namespace lp2_rec_ghosts.Model.Interfaces
+{
+    public interface IImmediateInteractle
+    {
+        public void OnImmediateInteraction();
+    }
+}
---
+++ Model/Interfaces/IPassiveInteracteable.cs
@@ -0,0 +1,8 @@
+namespace lp2_rec_ghosts.Model.Interfaces
+{
+
+    public interface IPassiveInteracteable
+    {
+         public void OnPassiveInteraction();
+    }
+}
