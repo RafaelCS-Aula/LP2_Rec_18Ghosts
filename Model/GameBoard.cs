680da67566f2105893995efae1a17808e9077b4a
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-07 02:04:23 +0000

    Define the game board programmaticly and update README

--- Model/GameBoard.cs
+++ Model/GameBoard.cs
@@ -2,43 +2,59 @@ using System.Collections.Generic;
 
 namespace lp2_rec_ghosts.Model
 {
-    public static class GameBoard
+    public class GameBoard
     {
+        /// <summary>
+        /// The gameBoard itself stored as a collection of its grid positions
+        /// and the corresponding objects.
+        /// </summary>
+        /// <typeparam name="Vector"> All the grid positions on the board
+        /// </typeparam>
+        /// <typeparam name="BoardObject[]"> An array of all the objects
+        /// that will be placed on the board at a specific grid position.
+        /// The index indicates the "layer" of that object with the first
+        /// layer [0] being the static objects on the board (tiles/walls)
+        /// the second [1] where the Ghosts are and the third [2] is
+        /// where the dummy portal ghosts are.</typeparam>
+        public Dictionary<Vector, BoardObject[]> Board 
+            {get; private set;} = new Dictionary<Vector, BoardObject[]>();
 
-        public static Dictionary<Vector, BoardObject[]> Board 
-            {get; private set;} 
+        // With (0,0) being the Bottom-Left of the screen  
+        // However in the console window it is the Top-Left  
 
-        // With (0,0) being the Bottom-Left of the screen    
-        public static void InitBoard()
+        /// <summary>
+        /// Creates the board in it's inicial state.
+        /// </summary>
+        public void BuildBoard()
         {
-
-            Board = new Dictionary<Vector, BoardObject[]>();
 
             // WALLS
 
-            // Bottom Horizontal Wall
-            for(int i = 0; i < 6; i++)
-            {
-                Board.Add(new Vector(i,i), new BoardObject[3]
-                {new CarpetTile(Colors.BLOCK), null, null});
-            }
-
-            // Left Vertical Wall
-            for(int i = 0; i < 6; i++)
-            {
-                Board.Add(new Vector(0,i), new BoardObject[3]
-                {new CarpetTile(Colors.BLOCK), null, null});
-            }
-
             // Top Horizontal Wall
-            for(int i = 0; i < 6; i++)
+            for(int i = 1; i < 6; i++)
+            {
+                Board.Add(new Vector(i,0), new BoardObject[3]
+                {new CarpetTile(Colors.BLOCK), null, null});
+            }
+
+            // Bottom Horizontal Wall
+            for(int i = 1; i < 6; i++)
             {
                 Board.Add(new Vector(i,6), new BoardObject[3]
                 {new CarpetTile(Colors.BLOCK), null, null});
             }
 
+            // Left Vertical Wall
+            for(int i = 0; i <= 6; i++)
+            {
+                Board.Add(new Vector(0,i), new BoardObject[3]
+                {new CarpetTile(Colors.BLOCK), null, null});
+            }
+
+
+
             // Right Vertical Wall
-            for(int i = 0; i < 6; i++)
+            for(int i = 0; i <= 6; i++)
             {
                 Board.Add(new Vector(6,i), new BoardObject[3]
                 {new CarpetTile(Colors.BLOCK), null, null});
@@ -59,8 +75,61 @@ namespace lp2_rec_ghosts.Model
                 {new CarpetTile(Colors.BLOCK), null, null});
 
 
-            
+            // CARPETS
 
+            // Top Row
+            Board.Add(new Vector(1,5), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+            Board.Add(new Vector(2,5), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+            Board.Add(new Vector(4,5), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+            Board.Add(new Vector(5,5), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+            
+            // 4th Row
+            Board.Add(new Vector(1,4), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
+            Board.Add(new Vector(2,4), new BoardObject[3]
+                {new CarpetTile(Colors.MIRROR), null, null});
+            Board.Add(new Vector(3,4), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
+            Board.Add(new Vector(4,4), new BoardObject[3]
+                {new CarpetTile(Colors.MIRROR), null, null});
+            Board.Add(new Vector(5,4), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
+
+            // 3rd Row
+            Board.Add(new Vector(1,3), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+            Board.Add(new Vector(2,3), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+            Board.Add(new Vector(3,3), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+            Board.Add(new Vector(4,3), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+
+            // 2nd Row
+            Board.Add(new Vector(1,2), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+            Board.Add(new Vector(2,2), new BoardObject[3]
+                {new CarpetTile(Colors.MIRROR), null, null});
+            Board.Add(new Vector(3,2), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
+            Board.Add(new Vector(4,2), new BoardObject[3]
+                {new CarpetTile(Colors.MIRROR), null, null});
+            Board.Add(new Vector(5,2), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+
+            // 1st Row
+            Board.Add(new Vector(1,1), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
+            Board.Add(new Vector(2,1), new BoardObject[3]
+                {new CarpetTile(Colors.RED), null, null});
+            Board.Add(new Vector(4,1), new BoardObject[3]
+                {new CarpetTile(Colors.BLUE), null, null});
+            Board.Add(new Vector(5,1), new BoardObject[3]
+                {new CarpetTile(Colors.YELLOW), null, null});
         }
     }
 }
