8fb7653988672df89f842fa34c2dc4bd336e6b78
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-10 05:19:59 +0000

    Start of console implementation of game model

---
+++ Console_Implementation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using lp2_rec_ghosts.Model;
+
+namespace lp2_rec_ghosts.Console_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            /*Console.Clear();
+            GameBoard.InitBoard();
+            foreach(var v in GameBoard.Board)
+            {
+                Console.SetCursorPosition(v.Key.X, v.Key.Y );
+                switch(v.Value[0].Color)
+                {
+                    case Colors.BLOCK:
+                        Console.BackgroundColor = ConsoleColor.White;
+                    break;
+                    case Colors.BLUE:
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    break;
+                    case Colors.YELLOW:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    break;
+                    case Colors.RED:
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                    break;
+                    case Colors.MIRROR:
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                    break;
+                }
+                
+                
+                
+                Console.Write(" ");
+
+            }
+
+            Console.SetCursorPosition(0, 10);
+            Console.BackgroundColor = ConsoleColor.Black;*/
+                
+            
+        }
+    }
+}
