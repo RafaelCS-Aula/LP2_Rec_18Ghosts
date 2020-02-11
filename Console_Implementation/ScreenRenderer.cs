da51cb40ad5790cc1fe47b7b5485208b9b91140f
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-11 06:24:58 +0000

    Imported Ghost and Dungeon List drawing from LPI project

--- Console_Implementation/ScreenRenderer.cs
+++ Console_Implementation/ScreenRenderer.cs
@@ -1,28 +1,73 @@
 using lp2_rec_ghosts.Model.BridgeClasses;
 using lp2_rec_ghosts.Model.Interfaces;
 using lp2_rec_ghosts.Model.Enums;
+using lp2_rec_ghosts.Model.Ghosts;
+using lp2_rec_ghosts.Model.GameTypes;
 using System;
+using System.Collections.Generic;
 
 namespace lp2_rec_ghosts.Console_Implementation
 {
     public class ScreenRenderer: IRenderer
     {
+        // Characters to be used for each element of the board.
+        private char tileVisual = '+';
+        private char mirrorVisual = 'M';
+        private char ghostPlayer1Visual = '@';
+        private char ghostPlayer2Visual = '&';
+        private char[] portalVisuals = {'←','→','↑','↓'};
+
+
         public void DrawMessage(string msg)
         {
-
+            
+            Console.SetCursorPosition(0,25);
+            Console.WriteLine("     ");
+            Console.WriteLine("     ");
+            Console.SetCursorPosition(0,25);
+            Console.Write(
+                $"Player{GameController.CurrentPlayer.playerNumber}: " + 
+                  msg +"\n");
 
         }
 
+        public void DrawGhostList(GhostObject[][] list)
+        {
 
+            DrawPlayerGhostList(GameController.CurrentPlayer, list);
+            DrawDungeonGhostList(list[3]);
+
+
+
+
+        }
+        public void DrawBoard(Dictionary<Vector, IBoardObject[]> board){}
+
+
+        /// <summary>
+        /// Draws numbers on the edges of the grid.
+        /// </summary>
+        /// <remarks> Taken from 'Linguagens de Programação I' 1st project.
+        /// </remarks>
+        private void DrawNumbers()
+        {
+            Console.Clear();
+
+            // Row
+            Console.WriteLine(" 01234");
+            // Column
+            Console.Write("0\n1\n2\n3\n4");
+        }
 
         /// <summary>
         /// Quick helper method to switch the color of the text.
         /// </summary>
         /// <param name="c"> Color to change it to </param>
+        ///<remarks> Taken from 'Linguagens de Programação I' 1st project.
+        ///</remarks>  
         private void TextColorSwitcher(Colors c)
         {
-            ///<remarks> Taken from 'Linguagens de Programação I' 1st project.
-            ///</remarks>     
+   
        
             switch(c)
             {
@@ -44,6 +89,105 @@ namespace lp2_rec_ghosts.Console_Implementation
             }        
         }
 
+        /// <summary>
+        /// Draws the List of Ghosts on scree.
+        /// </summary>
+        /// <param name="currentPlayer">  Variável do jogador no momento.
+        /// </param>
+        private void DrawPlayerGhostList(Player currentPlayer,
+             GhostObject[][] ghosts)
+        {
+            // Variável.
+            char c = ' ';
+            // Cor da consola.
+            Console.ForegroundColor = ConsoleColor.White;
+            // Acede a classe Ghost.
+            GhostObject g;
+
+            // Na posição (30,0) da janela da consola vai ser escrito 
+            // "Your Ghosts:".
+            Console.SetCursorPosition(30,0);
+            Console.Write("Your Ghosts:");
+
+            // Na posição (15,1) da janela da consola vai ser escrito 
+            // "Type the number on the left to select them.".
+            Console.SetCursorPosition(15,1);
+            Console.Write("Type the number on the left to select them.");
+
+            // Na posição (15,2) da janela da consola vai ser escrito 
+            // "Then type in grid coordinates to place them.\n".
+            Console.SetCursorPosition(15,2);
+            Console.Write("Then type in grid coordinates to place them.\n");
+            
+            // Modifica os visuais dos fantsmas dos jgoadores.
+            if (currentPlayer.playerNumber == 1) c = ghostPlayer1Visual;
+            else if (currentPlayer.playerNumber == 2) c = ghostPlayer2Visual;
+
+            // Conta quantos fantasmas o jogador tem.
+            for(int x = 0; x < 3; x++)
+            {
+                for (int i = 0; i < ghosts[x].Length; i++)
+                {
+                Console.CursorLeft = 25;
+                g = ghosts[x][i];
+                TextColorSwitcher(g.MyColor);
+                if(g != null)
+                    Console.Write(
+                        $"[{i}] - {c}, at ({g.Position.X},{g.Position.Y})\n");
+
+                }
+
+
+            }
+            
+        }
+
+        /// <summary>
+        /// Método para apresentar a lista da caverna dos fantasmas.
+        /// </summary>
+        /// <param name="dung"> Acede a classe Dungeon. </param>
+        public void DrawDungeonGhostList(GhostObject[] dung)
+        {
+            // Variável.
+            char c = ' ';
+            // Coloca a cor da consola a branco.
+            Console.ForegroundColor = ConsoleColor.White;
+            // Acede a classe Ghost.
+            GhostObject g;
+            
+            // Na posição (90, 0) é escrito "Your Ghosts:".
+            Console.SetCursorPosition(90,0);
+            Console.Write("Your Ghosts in the Dungeon:");
+
+            // Na posição (70, 1) é escrito "Type 'd' + the number on the 
+            // left to select them.".
+            Console.SetCursorPosition(70,1);
+            Console.Write("Type 'd' + the number on the left to select them.");
+
+            // Na posição (70, 2) é escrito "Selecting a ghost in the 
+            // Dungeon will give the other player.\n" .
+            Console.SetCursorPosition(70,2);
+            Console.Write("Selecting a ghost in the Dungeon will give" + 
+             "it to the other player.\n");
+
+            // Apresenta os fantasmas de cada jogador na caverna.
+            for (int i = 0; i < dung.Length; i++)
+            {
+                g = dung[i];
+                if (g.Owner.playerNumber == 1) c = ghostPlayer1Visual;
+                else if (g.Owner.playerNumber == 2) c = ghostPlayer2Visual;
+                TextColorSwitcher(g.MyColor);
+
+                Console.CursorLeft = 90;
+                Console.Write($"[{i}] - {c}.\n");
+            }
+
+            // Faz um tracejado a branco na coluna 70.
+            Console.CursorLeft = 70;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("---------------------------------------------" +
+                "-----------");
+        }
 
 
         
