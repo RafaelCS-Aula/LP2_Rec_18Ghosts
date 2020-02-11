eb8bb375ae3a0c349d88fdeeaa49d21e23ba27f3
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-11 19:17:25 +0000

    Add Model updates made in console branch

--- Assets/Scripts/Model/Enums/Colors.cs
+++ Assets/Scripts/Model/Enums/Colors.cs
@@ -1,17 +1,21 @@
+using System;
+
 namespace lp2_rec_ghosts.Model.Enums
 {
     /// <summary>
     /// The colors that characterize every element on the board. 
     /// </summary>
+    [Flags]
     public enum Colors
     {
         /// <remarks> Tiles that cannot be moved to and do not have a color 
         /// </remarks>
-        BLOCK = 0,
-        RED = 1,
-        YELLOW = 2,
-        BLUE = 4,
-        MIRROR = 6
+        NONE = 0,
+        BLOCK = 1,
+        RED = 2,
+        YELLOW = 4,
+        BLUE = 6,
+        MIRROR = 8
         
     }
 }
