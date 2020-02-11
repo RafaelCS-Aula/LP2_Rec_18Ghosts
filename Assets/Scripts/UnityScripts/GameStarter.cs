2398b7963789c54d35918bb83cca2844dca88e3d
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-02-11 03:05:22 +0000

    Create GameStarter class that starts the game.

---
+++ Assets/Scripts/UnityScripts/GameStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using lp2_rec_ghosts.Model.BridgeClasses;
+
+public class GameStarter : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject RenderControlPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController.SetupGame();
+        RenderControlPrefab.SetActive(true);
+    }
+
+    
+}
